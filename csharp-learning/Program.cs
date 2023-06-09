82c92cfd62de1dbe313ca375d7d2738c81b7de31
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-06-09 09:31:32 +0300

    Arrays

--- csharp-learning/Program.cs
+++ csharp-learning/Program.cs
@@ -2,43 +2,64 @@
 {
 	public class Program
 	{
+		// ---- FUNCTIONS ---------
+		static void PrintArray(int[] intArray,string mess)
+		{
+			foreach(int i in intArray)
+			{
+				Console.WriteLine($"{i} : {mess}");
+			}
+		}
+
+		// ---- END OF FUNCTIONS ---------
 		static void Main(string[] args)
 		{
-			// Strings
-			string randString = "This is a string";
-			Console.WriteLine(randString.Length);
-			Console.WriteLine(randString.Contains("is"));
-			Console.WriteLine(randString.IndexOf("is"));
-			Console.WriteLine(randString.Remove(10, 6));
-			Console.WriteLine(randString.Insert(10, "short"));
-			Console.WriteLine(randString.Replace("string", "sentence"));
-			Console.WriteLine(String.Compare("A", "B", StringComparison.OrdinalIgnoreCase));
-			Console.WriteLine("-------------------------");
+			int[] favoriteNums = new int[3];
+			favoriteNums[0] = 27;
+			Console.WriteLine($"fav num index 0 {favoriteNums[0]}");
+			string[] customers = { "Cosmo", "Mili", "Magda", "MyShU" };
+			var emplyees = new[] { "Paul", "Rick" };
+			object[] randomArray = { "Cosmo", 22, 1.234 };
+			for(int i = 0; i < randomArray.Length; i++)
+			{
+				Console.WriteLine($"Array : {i} : Value : {favoriteNums[i]}");
+			}
 
-			Console.WriteLine("A = 0 : {0}",
-				String.Equals("A", "a", 
-				StringComparison.OrdinalIgnoreCase));
-			Console.WriteLine("Pad Left : {0}",
-				randString.PadLeft(20, '.'));
-			Console.WriteLine("Pad Right : {0}",
-				randString.PadRight(20, '.'));
+			Console.WriteLine("--------------------");
+			int[,] matrixNum = new int[2, 2] { { 0, 1 }, { 0, 2 } };
+			for(int j = 0; j < matrixNum.GetLength(0); j++)
+			{
+				for(int k=0;k<matrixNum.GetLength(0); k++)
+				{
+					Console.Write($"{matrixNum[j, k]} ");
+				}
+				Console.WriteLine();
+			}
 
-			Console.WriteLine("Trim : {0}",
-				randString.Trim());
+			int[] randNums = { 1, 4, 9, 2 };
+			PrintArray(randNums, "Foreach");
 
-			Console.WriteLine("Trim : {0}",
-				randString.Trim());
+			Console.WriteLine("--------------------");
 
-			Console.WriteLine("Uppercase : {0}",
-				randString.ToUpper());
+			Array.Sort(randNums);
+			Console.WriteLine(randNums);
+			Array.Reverse(randNums);
+			Console.WriteLine(randNums);
+			Console.WriteLine($"1 at index {Array.IndexOf(randNums, 1)}");
+			randNums.SetValue(0, 1);
+			int[] srcArray = { 1, 2, 3, 4 };
+			int[] dstArray = new int[2];
+			int startInd = 0;
+			int length = 2;
+			Array.Copy(srcArray, startInd, dstArray, 0, length);
+			PrintArray(dstArray, "Copy");
+			Array anotherArray = Array.CreateInstance(typeof(int), 10);
+			srcArray.CopyTo(anotherArray, 5);
+			foreach(int m in anotherArray)
+			{
+				Console.WriteLine($"Copy to: {m}");
+			}
 
-			Console.WriteLine("Lowercase : {0}",
-				randString.ToLower());
-
-			string newString = string.Format("{0} saw a {1} {2} in the {3}",
-				"Paul", "rabbit", "eating", "field");
-			Console.WriteLine(newString);
-			Console.WriteLine(@"Exactly what I've typed \n");
 		}
 	}
 }
