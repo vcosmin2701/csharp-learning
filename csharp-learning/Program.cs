c2b0cf8aad1891fe63c7b72afcdaf5270eed4d4d
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-06-09 06:43:43 +0300

    Small introduction and console customization :D

--- csharp-learning/Program.cs
+++ csharp-learning/Program.cs
@@ -1,7 +1,16 @@
-﻿class Program
+﻿namespace Program
 {
-	public static void Main(String[] args)
+	public class Program
 	{
-		Console.WriteLine("IS THAT WORKING ??");
+		static void Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.Black;
+			Console.BackgroundColor = ConsoleColor.White;
+			Console.Clear();
+			Console.Write("What is your name ? ");
+			string name = Console.ReadLine();
+			Console.WriteLine($"Hello {name}!");
+
+		}
 	}
 }
