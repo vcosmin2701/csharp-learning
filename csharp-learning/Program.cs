da740d8fc239f58e85bf1a6542d38d526819188e
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-06-09 07:45:00 +0300

    Strings

--- csharp-learning/Program.cs
+++ csharp-learning/Program.cs
@@ -4,10 +4,41 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Currency : {0:c}", 23.455);
-			Console.WriteLine("Pad with 0s : {0:d4}", 23);
-			Console.WriteLine("3 decimals : {0:f3}", 23.45555);
-			Console.WriteLine("Commas : {0:n4}", 23.455);
+			// Strings
+			string randString = "This is a string";
+			Console.WriteLine(randString.Length);
+			Console.WriteLine(randString.Contains("is"));
+			Console.WriteLine(randString.IndexOf("is"));
+			Console.WriteLine(randString.Remove(10, 6));
+			Console.WriteLine(randString.Insert(10, "short"));
+			Console.WriteLine(randString.Replace("string", "sentence"));
+			Console.WriteLine(String.Compare("A", "B", StringComparison.OrdinalIgnoreCase));
+			Console.WriteLine("-------------------------");
+
+			Console.WriteLine("A = 0 : {0}",
+				String.Equals("A", "a", 
+				StringComparison.OrdinalIgnoreCase));
+			Console.WriteLine("Pad Left : {0}",
+				randString.PadLeft(20, '.'));
+			Console.WriteLine("Pad Right : {0}",
+				randString.PadRight(20, '.'));
+
+			Console.WriteLine("Trim : {0}",
+				randString.Trim());
+
+			Console.WriteLine("Trim : {0}",
+				randString.Trim());
+
+			Console.WriteLine("Uppercase : {0}",
+				randString.ToUpper());
+
+			Console.WriteLine("Lowercase : {0}",
+				randString.ToLower());
+
+			string newString = string.Format("{0} saw a {1} {2} in the {3}",
+				"Paul", "rabbit", "eating", "field");
+			Console.WriteLine(newString);
+			Console.WriteLine(@"Exactly what I've typed \n");
 		}
 	}
 }
