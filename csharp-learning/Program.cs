480ff6234cd260de5da3936303ea4d4cf4f0a55e
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-06-09 06:59:53 +0300

    MinValue and MaxValue of some types

--- csharp-learning/Program.cs
+++ csharp-learning/Program.cs
@@ -7,10 +7,17 @@
 			Console.ForegroundColor = ConsoleColor.Black;
 			Console.BackgroundColor = ConsoleColor.White;
 			Console.Clear();
-			Console.Write("What is your name ? ");
-			string name = Console.ReadLine();
-			Console.WriteLine($"Hello {name}!");
 
+			bool canIVote = true;
+			Console.WriteLine("Biggest Integer: {0}", int.MaxValue);
+			Console.WriteLine("Biggest Integer: {0}", int.MinValue);
+
+			Console.WriteLine("Biggest Long: {0}", long.MaxValue);
+			Console.WriteLine("Biggest Long: {0}", long.MinValue);
+
+			decimal decPiVal = 3.1415926535897932384626433832M;
+			decimal decBigNum = 3.00000000000000000000000000011M;
+			Console.WriteLine(decPiVal + decBigNum);
 		}
 	}
 }
