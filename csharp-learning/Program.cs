b6729e4dab7eb221b4b90bc18dc82fdefa9493fa
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-06-09 07:12:34 +0300

    Data conversion

--- csharp-learning/Program.cs
+++ csharp-learning/Program.cs
@@ -4,20 +4,19 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.ForegroundColor = ConsoleColor.Black;
-			Console.BackgroundColor = ConsoleColor.White;
-			Console.Clear();
+			bool boolFromString = bool.Parse("true");
+			int intFromString = int.Parse("100");
+			double doubleFromString = double.Parse("1.234");
 
-			bool canIVote = true;
-			Console.WriteLine("Biggest Integer: {0}", int.MaxValue);
-			Console.WriteLine("Biggest Integer: {0}", int.MinValue);
+			string stringVal = doubleFromString.ToString();
+			Console.WriteLine(stringVal.GetType());
 
-			Console.WriteLine("Biggest Long: {0}", long.MaxValue);
-			Console.WriteLine("Biggest Long: {0}", long.MinValue);
+			double doubleNumber = 12.345;
+			Console.WriteLine($"{(int)doubleNumber}");
 
-			decimal decPiVal = 3.1415926535897932384626433832M;
-			decimal decBigNum = 3.00000000000000000000000000011M;
-			Console.WriteLine(decPiVal + decBigNum);
+			int intNum = 10;
+			long longNum = intNum;
+			
 		}
 	}
 }
