bccff79f55af1ad8ec4f21ef2861d1f53ca56257
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-06-12 14:26:28 +0300

    Getters/Setters Properties and readonly fields

--- csharp-learning/Animal.cs
+++ csharp-learning/Animal.cs
@@ -8,29 +8,24 @@ namespace csharp_learning
 {
 	class Animal
 	{
-		public string name;
-		public string sound;
-		static int numberOfAnimals = 0;
+		private string name;
+		private string sound;
+		public static int numberOfAnimals = 0;
 
-		public Animal()
-		{
-			name = "No Name";
-			sound = "No Sound";
-			numberOfAnimals++;
-		}
+		public const string SHELTER = "Cosmo's farm";
+		public readonly int idNum;
 
-		public Animal(string name = "No name")
-		{
-			this.name = name;
-			this.sound = "No sound";
-			numberOfAnimals++;
-		}
+		public Animal():this("No name", "No Sound") {  }
 
-		public Animal(string name = "No Name", string sound = "No Sound")
+		public Animal(string name) : this(name, "No Sound") { }
+
+		public Animal(string name, string sound)
 		{
-			this.name = name;
-			this.sound = sound;
-			numberOfAnimals++;
+			SetName(name);
+			Sound = sound;
+			NumOfAnimals = 1;
+			Random rand = new Random();
+			idNum = rand.Next(1, 214748640);
 		}
 
 		public void MakeSound()
@@ -41,6 +36,50 @@ namespace csharp_learning
 		public static int GetNumAnimals()
 		{
 			return numberOfAnimals;
+		}
+
+		public void SetName(string name)
+		{
+			if (!name.Any(char.IsDigit))
+			{
+				this.name = name;
+			}
+			else
+			{
+				this.name = "No name";
+				Console.WriteLine("Invalid input name(can't contain digits");
+			}
+		}
+
+		public string GetName()
+		{
+			return name;
+		}
+
+		public string Sound
+		{
+			get { return sound; }
+			set
+			{
+				if(value.Length > 10)
+				{
+					sound = "No sound";
+					Console.WriteLine("Sound is too long");
+				}
+				else
+				{
+					sound = value;
+				}
+			}
+		}
+
+		public string Owner { get; set; } = "No Owner";
+
+		public static int NumOfAnimals
+		{
+			get { return numberOfAnimals;  } 
+			set { numberOfAnimals += value; }
+
 		}
 	}
 }
--- csharp-learning/Program.cs
+++ csharp-learning/Program.cs
@@ -10,52 +10,14 @@ namespace Program
 	{
 		static void Main(string[] args)
 		{
-			Rectangle rect1;
-			rect1.length = 200;
-			rect1.width = 50;
-			Console.WriteLine($"Area of rect1: {rect1.Area()}");
-			Rectangle rect2 = new Rectangle(100, 40);
-			rect2 = rect1;
-			rect1.length = 33;
-			Console.WriteLine($"Rect2.length: {rect2.length}");
-
-
-			Animal fox = new Animal()
-			{
-				name = "Red",
-				sound = "Rwaaa"
-			};
-
-			Console.WriteLine($"number of animals: #{Animal.GetNumAnimals()}");
-			Console.WriteLine($"Area of Rectangle: {ShapeMath.GetArea("rectangle", 5, 6)}");
-
-			int? randNum = null;
-			if (randNum == null)
-			{
-				Console.WriteLine("randNum is null");
-			}
-
-			if (!randNum.HasValue)
-			{
-				Console.WriteLine("randNum is null");
-			}
-
-		}
-		struct Rectangle
-		{
-			public double length;
-			public double width;
-
-			public Rectangle(double l = 1, double w = 1)
-			{
-				length = l;
-				width = w;
-			}
-
-			public double Area()
-			{
-				return length * width;
-			}
+			Animal cat = new Animal();
+			cat.SetName("Marty");
+			cat.Sound = "Meow";
+			Console.WriteLine($"The cat is named {cat.GetName()} and says {cat.Sound}");
+			cat.Owner = "Cosmo";
+			Console.WriteLine($"{cat.GetName()} owner is {cat.Owner}");
+			Console.WriteLine($"{cat.GetName()} shelter id is {cat.idNum}");
+			Console.WriteLine($"# number of animals is {Animal.numberOfAnimals}");
 		}
 	}
 }
