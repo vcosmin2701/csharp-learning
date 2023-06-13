70489a9158a78e835d38875fcf46e58e0b646721
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-06-13 09:45:36 +0300

    Interfaces

--- grindingCsharp/Program.cs
+++ grindingCsharp/Program.cs
@@ -4,27 +4,16 @@
 	{
 		static void Main(string[] args)
 		{
-			Shape[] shapes = { new Circle(5), new Rectangle(4, 5) };
-			foreach(Shape s in shapes)
+			Vehicle car1 = new Vehicle("Porsche Carrera GT", 4, 208);
+			if(car1 is IDrivable)
 			{
-				s.GetInfo();
-				Console.WriteLine("{0} Area : {1:f2}", s.Name, s.Area());
-
-				Circle? testCirc = s as Circle;
-				if(testCirc == null)
-				{
-					Console.WriteLine("This is not a circle");
-				}
-
-				if(s is Circle)
-				{
-					Console.WriteLine("This isn't a Rectangle");
-				}
+				car1.Move();
+				car1.Stop();
 			}
-
-			object circ1 = new Circle(4);
-			Circle circ2 = (Circle)circ1;
-			Console.WriteLine("The {0} Area is {1:f2}", circ2.Name, circ2.Area());	
+			else
+			{
+				Console.Write("This is faulty {0}", car1.Brand);
+			}
 		}
 	}
 }
---
+++ grindingCsharp/Vehicle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grindingCsharp
+{
+	class Vehicle : IDrivable
+	{
+		public string Brand { get; set; }
+		public Vehicle(string brand = "No Brand",
+			int wheels = 0,
+			double speed = 0)
+		{
+			Brand = brand;
+			Wheels = wheels;
+			Speed = speed;
+
+		}
+
+		public double Speed { get; set; }
+		public int Wheels { get; set; }
+
+		public void Move()
+		{
+			Console.WriteLine($"The {Brand} Moves Forward at {Speed} MPH");
+		}
+
+		public void Stop()
+		{
+			Console.WriteLine($"The {Brand} Stops");
+		}
+	}
+}
